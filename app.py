0ac8e83149af7250a10a138cd5414655d47c2bba
Par-Dodo <87794669+Par-Dodo@users.noreply.github.com>
2022-03-02 20:35:47 -0800

    app basics

--- app.py
+++ app.py
@@ -1,98 +1,69 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "5813f2ab",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import requests\n",
-    "import pandas as pd\n",
-    "import numpy as np\n",
-    "import psycopg2\n",
-    "import sqlite3\n",
-    "import sqlalchemy\n",
-    "import csv\n",
-    "import time\n",
-    "from flask import Flask, render_template, redirect\n",
-    "from flask import Flask, redirect, url_for, request\n",
-    "from flask_sqlalchemy import SQLAlchemy\n",
-    "from flask import Response\n",
-    "from flask import request\n",
-    "from sqlalchemy.ext.automap import automap_base\n",
-    "from sqlalchemy.orm import Session, session\n",
-    "from sqlalchemy import create_engine, func\n",
-    "from sqlalchemy_utils import database_exists, create_database\n",
-    "from flask import Flask, jsonify"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 6,
-   "id": "249d3fa7",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "/opt/anaconda3/lib/python3.8/site-packages/flask_sqlalchemy/__init__.py:872: FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future.  Set it to True or False to suppress this warning.\n",
-      "  warnings.warn(FSADeprecationWarning(\n"
-     ]
-    }
-   ],
-   "source": [
-    "app = Flask(__name__)\n",
-    "app.config['SQLALCHEMY_DATABASE_URI'] = \"sqlite:////tmp/project4\"\n",
-    "sql = SQLAlchemy(app)\n",
-    "\n",
-    "# Creating Engine\n",
-    "engine = create_engine(\"sqlite:////tmp/project4\")\n",
-    "\n",
-    "# reflecting database\n",
-    "Base = automap_base()\n",
-    "# reflecting tables\n",
-    "Base.prepare(engine, reflect=True)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "8f0536ba",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "@app.route('/', methods =[\"GET\", \"POST\"])\n",
-    "def input():\n",
-    "    if request.method == 'POST':\n",
-    "      year = request.form['year']\n",
-    "      return redirect(f'migration_data_2019/{year}')\n",
-    "    else:\n",
-    "        year = request.args.get('year')\n",
-    "        return render_template('form.html')"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.12"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+import numpy as np
+import sqlalchemy
+import csv
+import time
+import pickle
+from config import password
+from decimal import Decimal
+from flask import Flask, render_template, redirect
+from flask import Flask, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask import Response
+from flask import request
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session, session
+from sqlalchemy import create_engine, func
+from sqlalchemy_utils import database_exists, create_database
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:{password}@localhost:5432/project4'
+sql = SQLAlchemy(app)
+
+# Creating Engine
+engine = create_engine(f"postgresql://postgres:{password}@localhost:5432/project4")
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+# reflecting database
+Base = automap_base()
+
+# reflecting tables
+Base.prepare(engine, reflect=True)
+
+# Load ML model
+model = pickle.load(open('model.pkl', 'rb')) 
+
+# Create application
+app = Flask(__name__)
+
+# Bind home function to URL
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Bind predict function to URL
+@app.route('/predict', methods =['POST'])
+def predict():
+    
+    # Put all form entries values in a list 
+    features = [float(i) for i in request.form.values()]
+    # Convert features to array
+    array_features = [np.array(features)]
+    # Predict features
+    prediction = model.predict(array_features)
+    
+    output = prediction
+    
+    # Check the output values and retrive the result with html tag based on the value
+    if output == 1:
+        return render_template('index.html', 
+                               result = 'The patient is not likely to have heart disease!')
+    else:
+        return render_template('index.html', 
+                               result = 'The patient is likely to have heart disease!')
+
+#Run the application
+if __name__ == "__main__":
+    app.run(debug=True)
